f62e3b4f680c96c5c2cfb324a5ef42bc4de410fd
HungryLim <36051245+HungryLim@users.noreply.github.com>
2018-08-06 11:40:28 -0500

    Lim
    lim

--- day01/exercise01_fib.py
+++ day01/exercise01_fib.py
@@ -4,3 +4,26 @@
 
 ## Write a for loop, while loop, or function (or all three!) to create a
 ## list of the first 10 numbers of the fibonacci sequence
+
+    
+def fibo(n):
+    x=[1,1]
+    while len(x)<n:
+        num=x[len(x)-1]+x[len(x)-2] #first one is the last one and the second one is the first one
+        x.append(num)
+    return x
+
+fibo(10)
+fibo(20)
+
+    
+def fibo2(n):
+    x=[1,1]
+    while len(x)<n:
+        #first one is the last one and the second one is the first one
+        x.append(x[-1]+x[-2])
+    return x
+
+fibo2(10)
+
+
