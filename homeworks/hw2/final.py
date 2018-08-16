0bd6ebd2067a7d1f83be55cd402736a6e526b15a
HungryLim <36051245+HungryLim@users.noreply.github.com>
2018-08-16 13:35:12 -0500

    final update
    final

--- homeworks/hw2/final.py
+++ homeworks/hw2/final.py
@@ -9,9 +9,8 @@ import os
 os.chdir("C:/Users/wooki/Documents/GitHub/pythoncourse2018/homeworks/hw2")
 
 with open('hw2_lim.csv', 'wb') as f:
-	w = csv.DictWriter(f, fieldnames = ("Title", "Published date", "Issues", "Number of signatures"))
-	w.writeheader()
-
+    w = csv.DictWriter(f, fieldnames = ("Title", "Published date", "Issues", "Number of signatures"))
+    w.writeheader()
     still_petitions = True
     i=0
     while still_petitions:
@@ -19,44 +18,30 @@ with open('hw2_lim.csv', 'wb') as f:
         i += 1
         web_page = urllib2.urlopen(web_address)
         all_html = BeautifulSoup(web_page.read())
-        #page= all_html.find('div', {'class':"page-load-next"}).text
-        #print page
-    
         all_petition = all_html.find_all("h3")
         for p in all_petition[3:] :
             petition = {} ## empty dictionary to fill in
-            petition["Title"] = p.get_text()
+            petition["Title"] = p.get_text().encode("utf-8")
             address = p.find('a')
             extension = address["href"]
-                
             #date
             pet_page = urllib2.urlopen('https://petitions.whitehouse.gov%s' % extension)
             petition_html = BeautifulSoup(pet_page.read())
-	        fordate= petition_html.find('h4', {'class': "petition-attribution"})
+            fordate= petition_html.find('h4', {'class': "petition-attribution"})
             pre= fordate.text.strip()
-            petition["Published date"] =pre.split("on")[1]
+            petition["Published date"] =pre.split("on")[1].encode("utf-8")
 
             #issue
             issue= petition_html.find('div', {'class': "field-item even"})
-            petition["Issues"] =issue.text.strip()
+            petition["Issues"] =issue.text.strip().encode("utf-8")
 
             #Number of signature
             num_sig= petition_html.find("span", {'class':"signatures-number"})
-            petition["Number of signatures"] =num_sig.text.strip()
+            petition["Number of signatures"] =num_sig.text.strip().encode("utf-8")
 
             ## write observation to csv
             w.writerow(petition)
         try:
-            print i
             page= all_html.find('div', {'class':"page-load-next"}).text
         except AttributeError:
             still_petitions = False
-
-
-
-
-
-petition= ["a","b","c"]
-for p in petition[2:]:
-
-    print p
